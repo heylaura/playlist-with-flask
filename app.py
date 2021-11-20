dc3bfec00eb21c6533b8a04fc80087dec3187767
Laura Caroline <silva.lauracarolineadv@gmail.com>
2021-11-20 22:10:02 +0000

    Complemento de funções.

--- app.py
+++ app.py
@@ -16,6 +16,14 @@ lista_musicas = [
     {"musica": "Born to die", "artista": "Lana Del Rey", "genero": "Indie"},
     {"musica": "Como os nossos pais", "artista": "Elis Regina", "genero": "MPB"},
     {"musica": "Rindo à toa", "artista": "Falamansa", "genero": "Forró"},
+    {"musica": "Black", "artista": "Pearl Jam", "genero": "Rock"},
+    {"musica": "Runaway", "artista": "Aurora", "genero": "Indie"},
+    {"musica": "Save your tears", "artista": "The Weeknd", "genero": "Pop"},
+    {"musica": "Sorri, Sou Rei", "artista": "Natiruts", "genero": "Reggae"},
+    {"musica": "Don't let me down", "artista": "The Chainsmokers", "genero": "Eletrônica"},
+    {"musica": "Bohemian Rhapsody", "artista": "Queen", "genero": "Rock"},
+    {"musica": "Livin' on a Prayer", "artista": "Bon Jovi", "genero": "Rock"},
+    {"musica": "Cheia de manias", "artista": "Raça Negra", "genero": "Samba"},
 ]
 
 
@@ -70,21 +78,20 @@ def pesquisar():
     item = request.form["pesquisar"]
 
     for objeto in lista_musicas:
-
-        if item in objeto["musica"]:
-            lista_busca.append(objeto)
-                
-        if item in objeto["artista"]:
-            lista_busca.append(objeto)
-            
-        if item in objeto["genero"]:
+                       
+        if item.lower() in objeto["musica"].lower():
             lista_busca.append(objeto)
         
-        #else:
-            #return render_template('notfound.html')
+                
+        if item.lower() in objeto["artista"].lower():
+            lista_busca.append(objeto)
+        
+        
+        if item.lower() in objeto["genero"].lower():
+            lista_busca.append(objeto)
+
 
     return render_template('search.html', lista_busca=lista_busca)
-    
 
 
 
